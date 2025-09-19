c7629e44dcd60be38eec6caff9728414f297855e
Harry Dhillon <hdhillon478@gmail.com>
2025-09-19 18:02:53 +0100

    feat: add Navbar component with logo and menu images

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,16 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <img
+        src='/images/nav-logo.svg'
+        alt='navbar logo for gta 6'
+        className='scale-90'
+      />
+      <img
+        src='/images/menu.svg'
+        alt='navbar logo for gta 6 navigation menu'
+        className='w-10'
+      />
+    </nav>
+  );
+}

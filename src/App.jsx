0884d348256000ff064849a78aa5e7309f112e5a
Harry Dhillon <hdhillon478@gmail.com>
2025-09-25 11:53:12 +0100

    feat: add Jason component to display character information

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { ScrollTrigger } from "gsap/all";
 import Navbar from "./components/navbar";
 import Hero from "./components/sections/hero";
 import Intro from "./components/sections/intro";
+import Jason from "./components/sections/jason";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -13,6 +14,7 @@ export default function App() {
       <Navbar />
       <Hero />
       <Intro />
+      <Jason />
     </main>
   );
 }
